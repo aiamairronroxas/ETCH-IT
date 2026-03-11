115b015524314467a94208665d48e46f0437f549
aiamairronroxas <2022102810@ms.bulsu.edu.ph>
2026-03-11 21:25:17 +0800

    v16: changed the front-end, added one button

--- src/config.py
+++ src/config.py
@@ -8,16 +8,25 @@ COLORS = {
     "nav_hover": "#F0F0F0",
     "accent_red": "#9a1e1a",
     "btn_hover_red": "#880505",
+
     "f1_grad_start": "#b81d1d",
     "f1_grad_end": "#f04135",
+
     "f2_grad_start": "#f68519",
     "f2_grad_end": "#ffe439",
     "f2_hover_start": "#e0740d",
     "f2_hover_end": "#f0d320",
+
     "f3_grad_start": "#1a73e8",
     "f3_grad_end": "#6fb1fc",
     "f3_hover_start": "#155db1",
     "f3_hover_end": "#63a0e6",
+
+    "f4_grad_start": "#1f9d55",
+    "f4_grad_end": "#6ee7b7",
+    "f4_hover_start": "#187a43",
+    "f4_hover_end": "#57c79d",
+
     "text_primary": "#333333",
     "text_secondary": "#666666",
     "btn_text_white": "#FFFFFF",
@@ -26,19 +35,49 @@ COLORS = {
 
 FRAME_CONFIGS = {
     "f2_btn": {
-        "width": 440, "height": 400, "pos_x": 90, "pos_y": 600,
-        "corner_radius": 15, "text": "View Gerber", "text_color": "#FFFFFF",
-        "font": ("Arial", 25, "bold"), "icon_path": "assets/upload.png",
-        "icon_size": (35, 35), "spacing": 15
+        "width": 0,
+        "height": 400,
+        "pos_x": 0,
+        "pos_y": 600,
+        "corner_radius": 15,
+        "text": "View Gerber",
+        "text_color": "#FFFFFF",
+        "font": ("Arial", 25, "bold"),
+        "icon_path": "assets/upload.png",
+        "icon_size": (35, 35),
+        "spacing": 15
     },
     "f3_btn": {
-        "width": 440, "height": 400, "pos_x": 550, "pos_y": 600,
-        "corner_radius": 15, "text": "Etch-IT!", "text_color": "#FFFFFF",
-        "font": ("Arial", 25, "bold"), "icon_path": "assets/cnc.png",
-        "icon_size": (35, 35), "spacing": 15
+        "width": 0,
+        "height": 400,
+        "pos_x": 0,
+        "pos_y": 600,
+        "corner_radius": 15,
+        "text": "Etch",
+        "text_color": "#FFFFFF",
+        "font": ("Arial", 25, "bold"),
+        "icon_path": "assets/cnc.png",
+        "icon_size": (35, 35),
+        "spacing": 15
+    },
+    "f4_btn": {
+        "width": 0,
+        "height": 400,
+        "pos_x": 0,
+        "pos_y": 600,
+        "corner_radius": 15,
+        "text": "Drill",
+        "text_color": "#FFFFFF",
+        "font": ("Arial", 25, "bold"),
+        "icon_path": "assets/cnc.png",
+        "icon_size": (35, 35),
+        "spacing": 15
     },
     "status": {
-        "width": 380, "height": 840, "pos_x": 1020, "pos_y": 160,
+        "width": 380,
+        "height": 840,
+        "pos_x": 1020,
+        "pos_y": 160,
         "corner_radius": 20
     },
     "standalone_icon": {
